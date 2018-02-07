4201f89de8a1b83dd1163475180d56c1d51c2fdb
Wasim Alatrash <w-elatrash@hotmail.com>
2018-02-07 08:59:44 +0200

    Initial Commit

---
+++ SampleCalender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleCalender
+//
+//  Created by Wasim Alatrash on 2/7/18.
+//  Copyright © 2018 Wasim Alatrash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
